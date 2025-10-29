0f0c8b0642c0388384ca448f988fc5073a8dcdd8
kakkao71 <kakkao73@gmail.com>
2025-10-29 19:29:20 +0700

    solveendgame

--- Assets/Script/Story/PowerManager.cs
+++ Assets/Script/Story/PowerManager.cs
@@ -1,22 +1,48 @@
 using UnityEngine;
+using ARKOM.Core; // for EventBus
 
 namespace ARKOM.Story
 {
  [AddComponentMenu("Story/Power Manager")]
  public class PowerManager : MonoBehaviour
  {
+ [Header("Light Groups")]
  public Light[] normalLights;
  public Light[] emergencyLights;
+
+ [Header("Debug")] public bool logChanges = false;
+
+ private bool currentOn;
+
+ private void OnEnable()
+ {
+ EventBus.Subscribe<PowerRestoredEvent>(_ => SetPower(true));
+ EventBus.Subscribe<BlackoutStartedEvent>(_ => SetPower(false));
+ }
+ private void OnDisable()
+ {
+ EventBus.Unsubscribe<PowerRestoredEvent>(_ => SetPower(true));
+ EventBus.Unsubscribe<BlackoutStartedEvent>(_ => SetPower(false));
+ }
+
  public void SetPower(bool on)
  {
+ currentOn = on;
  if (normalLights != null)
  {
- foreach (var l in normalLights) if (l) l.enabled = on;
+ for (int i =0; i < normalLights.Length; i++)
+ {
+ var l = normalLights[i]; if (!l) continue; l.enabled = on;
+ }
  }
  if (emergencyLights != null)
  {
- foreach (var l in emergencyLights) if (l) l.enabled = !on;
+ for (int i =0; i < emergencyLights.Length; i++)
+ {
+ var l = emergencyLights[i]; if (!l) continue; l.enabled = !on;
  }
+ }
+ if (logChanges) Debug.Log($"[PowerManager] Power {(on ? "ON" : "OFF")} -> normal={normalLights?.Length ??0}, emergency={emergencyLights?.Length ??0}", this);
  }
  }
 }
